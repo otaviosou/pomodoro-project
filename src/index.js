059d97b2fa526911b6afc4c36369472fc0daed2f
Otavio Sousa <otavlio@outlook.com>
2020-05-25 18:49:06 -0700

    criado o arquivo raiz de js

---
+++ src/index.js
@@ -0,0 +1,6 @@
+let controller = new Controller()
+
+
+document.querySelector('#controlls #play').addEventListener('click', controller.play.bind(controller))
+document.querySelector('#controlls #pause').addEventListener('click', controller.pause.bind(controller))
+document.querySelector('#controlls #reset').addEventListener('click', controller.reset.bind(controller))
